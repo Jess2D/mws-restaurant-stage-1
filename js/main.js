0369e75ff761554746d3e33c5f6af1f6f3d69b69
jess2d <jessicap_silveira@hotmail.com>
2019-04-11 21:11:46 +1200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiamVzc3BybyIsImEiOiJjanVhejNwNWowODFlNDNwcWI0NHpyeWppIn0.VTJ6uYSr1YH__9x8zTPtwg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
