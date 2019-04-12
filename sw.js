a06d426fcb00f824e4398a32eb426c7eab06346e
jess2d <jessicap_silveira@hotmail.com>
2019-04-12 20:54:55 +1200

    Offline Availability

---
+++ sw.js
@@ -0,0 +1,28 @@
+const staticCache = 'restaurant-static-001';
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('staticCache').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/css/styles.css',
+       '/js/dbhelper.js',
+       '/js/register_sw.js',
+       '/js/main.js',
+       '/js/restaurant_info.js',
+       '/data/restaurants.json',
+       '/restaurant.html?id=1',
+       '/restaurant.html?id=2',
+       '/restaurant.html?id=3',
+       '/restaurant.html?id=4',
+       '/restaurant.html?id=5',
+       '/restaurant.html?id=6',
+       '/restaurant.html?id=7',
+       '/restaurant.html?id=8',
+       '/restaurant.html?id=9',
+       '/restaurant.html?id=10',
+     ]);
+   })
+ );
+});
